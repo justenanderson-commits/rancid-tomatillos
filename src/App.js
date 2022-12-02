8f13fefc9fb234c663d3e59ac21da986b73516e6
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-12-01 17:15:52 -0700

    Fix import problems with component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AllMovies from "./AllMovies";
+import AllMovies from "./components/AllMovies";
 
 function App() {
   return (
